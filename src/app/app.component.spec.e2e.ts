fb245207bd8fdc75dfd80e8d16669c41243fc667
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-07-01 15:21:28 +0200

    relative url in protractor tests

--- src/app/app.component.spec.e2e.ts
+++ src/app/app.component.spec.e2e.ts
@@ -2,13 +2,13 @@
 
 describe('Check Welcome Page', function() {
   it('Header', function() {
-    browser.get('http://localhost:4300');
+    browser.get('/');
     var header=element(by.tagName("h1"));
     expect(header.getCssValue("color")).toBe("rgba(255, 0, 0, 1)");
     expect(header.getText()).toBe("Demo!");
   });
   it('Bootstrap', function() {
-    browser.get('http://localhost:4300');
+    browser.get('/');
     var button=element(by.tagName("button"));
     expect(button.getCssValue("background-color")).toBe("rgba(51, 122, 183, 1)");
   });
