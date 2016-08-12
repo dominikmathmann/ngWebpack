d3d34b35c050b8d9f64ff7574f07ee8060075c87
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-08-12 10:47:18 +0200

    add prod mode

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import {LocationStrategy, Location, HashLocationStrategy } from '@angular/common
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { BrowserModule}  from '@angular/platform-browser';
-import { enableProdMode, provide, NgModule, ANALYZE_FOR_ENTRY_COMPONENTS} from '@angular/core';
-
-@NgModule({
-    imports: [ BrowserModule ],
-  providers: [APP_ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
-})
-class MyAppModule {}
-
-// JIT compile long form
+import { enableProdMode, provide, NgModule} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
+@NgModule({
+    imports: [BrowserModule],
+    providers: [APP_ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+class MyAppModule { }
+
+declare var process: any
+
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(MyAppModule);
